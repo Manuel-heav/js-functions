3d942dc213e7a2835c7ef387d8b454e8818f4b6b
manuel-heav <manuelheav32@gmail.com>
2022-07-05 17:04:19 +0300

    hoisting

---
+++ 01-hoisting-function-definition.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return a + b;
+}
+
+add(2, 2);
+
+// Hositing: Whenever your code runs javascript will do an initial scan of the whole code
+// so even if the function was declared before the function definition the snipper will run
