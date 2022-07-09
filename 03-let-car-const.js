7fe49932780213f506b9f23d3ed3f0f6448ce888
manuel-heav <manuelheav32@gmail.com>
2022-07-09 18:54:31 +0300

    let, var and const

--- 03-let-car-const.js
+++ 03-let-car-const.js
@@ -5,12 +5,21 @@
 // A variable without a value is undefined
 // when using var before intiialized the error will just be undefined...but with let and const the error will be cannot access "variable" before initialization.
 
-// var is hoisted, value undefined is logged
-
-// Hosisted mean whenever a variable is initialized js will take the the variable and assign it at the top
+// Hoisted mean whenever a variable is initialized js will take the the variable and assign it at the top
 
 // like var result = 5;
 // then js will take var result; and initialize it on the top under the hood
+
+// var is hoisted, value undefined is logged
+function doTheThing() {
+  if (true) {
+    console.log("yeah");
+  }
+  var result = 43;
+}
+
+// let is hoisted ...but we cant use it before initialization
+
 function doTheThing() {
   if (true) {
     console.log("yeah");
