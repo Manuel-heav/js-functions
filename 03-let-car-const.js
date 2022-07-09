148b13e5034127142d99c586821daced83a04c46
manuel-heav <manuelheav32@gmail.com>
2022-07-09 18:54:29 +0300

    let, var and const

--- 03-let-car-const.js
+++ 03-let-car-const.js
@@ -1,8 +1,16 @@
 // in modern javascript, don't use var...
-// 1. hoisted to the top of scope may introduce indefined variable bugs.
+// 1. hoisted to the top of scope may introduce undefined variable bugs.
 // 2.
 
 // A variable without a value is undefined
+// when using var before intiialized the error will just be undefined...but with let and const the error will be cannot access "variable" before initialization.
+
+// var is hoisted, value undefined is logged
+
+// Hosisted mean whenever a variable is initialized js will take the the variable and assign it at the top
+
+// like var result = 5;
+// then js will take var result; and initialize it on the top under the hood
 function doTheThing() {
   if (true) {
     console.log("yeah");
