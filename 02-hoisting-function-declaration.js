654554da619eeaf004f431ca15b40fde026306df
manuel-heav <manuelheav32@gmail.com>
2022-07-09 17:10:57 +0300

    hoisting declarartion updated

--- 02-hoisting-function-declaration.js
+++ 02-hoisting-function-declaration.js
@@ -1,5 +1,7 @@
 // Variable hoisting, the console log wont because there is no variable hoisting
-console.log(add(2, 2));
+console.log(add(2, 2)); // Error! Add is not a function(undefined)
 const add = function (a, b) {
   return a + b;
 };
+
+// Js will only hoist the variable not the assignment
