f2f70ba0f23dfb2ad1f540db3ba96b3c6da84412
manuel-heav <manuelheav32@gmail.com>
2022-07-14 19:25:49 +0300

    scope

---
+++ 04-scope.js
@@ -0,0 +1,9 @@
+// when and where our variables are accessible - Scope
+//  Levels of scope
+// 1. Global scope -
+// window object in the browser
+// global
+// If it is global...accessible anywhere
+// 2. File scope / module scope
+// accessibel anywhere in the current module or file
+// 3. Function scope
